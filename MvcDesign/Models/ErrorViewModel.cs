f038d6c70e84902da4b20bd8e9fdb19e39743025
aydinmfatih <aydinmfatih@gmail.com>
2023-07-21 17:31:48 +0300

    Add project files.

---
+++ MvcDesign/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcDesign.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MvcDesign/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MvcDesign.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Mail { get; set; }
+        public string Display { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto:{Mail}");
+            output.Content.Append(Display);
+        }
+    }
+}
