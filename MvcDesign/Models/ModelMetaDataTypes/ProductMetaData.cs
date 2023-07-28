bdfdb6940f8341bccfa749d784a9d7c0f9441764
aydinmfatih <aydinmfatih@gmail.com>
2023-07-28 10:28:04 +0300

    ModelDataType

---
+++ MvcDesign/Models/ModelMetaDataTypes/ProductMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDesign.Models.ModelMetaDataTypes
+{
+    public class ProductMetaData
+    {
+        [Required(ErrorMessage = "Product name Giriniz")]
+        [StringLength(100, ErrorMessage = "en fazla 100 karakter giriniz")]
+        public string ProductName { get; set; }
+        [EmailAddress(ErrorMessage = "geçerli bir adres giriniz")]
+        public string Email { get; set; }
+    }
+}
--- MvcDesign/Models/Product.cs
+++ MvcDesign/Models/Product.cs
@@ -1,15 +1,15 @@
-﻿using MvcDesign.Constants;
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcDesign.Constants;
+using MvcDesign.Models.ModelMetaDataTypes;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcDesign.Models
 {
+    [ModelMetadataType(typeof(ProductMetaData))]
     public class Product
     {
-        [Required(ErrorMessage ="Product name Giriniz")]
-        [StringLength(100,ErrorMessage ="en fazla 100 karakter giriniz")]
         public string ProductName { get; set; }
         public int Quantity { get; set; }
-        [EmailAddress(ErrorMessage ="geçerli bir adres giriniz")]
         public string Email { get; set; }
     }
 }
