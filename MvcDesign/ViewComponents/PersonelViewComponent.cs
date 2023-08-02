a74771a2e41466451a5211721f41c62d7a92678e
aydinmfatih <aydinmfatih@gmail.com>
2023-08-02 13:42:35 +0300

    ViewComponent

---
+++ MvcDesign/ViewComponents/PersonelViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcDesign.ViewComponents
+{
+    public class PersonelViewComponent:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
